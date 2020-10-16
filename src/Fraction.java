b9b1c48b9d8db12dffa3b49a944e9408ade4c2ba
Wissem-Sassi <sassiwissem97@gmail.com>
2020-10-16 15:15:34 +0200

    creation du constructeur et de la methode toString

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,7 +1,14 @@
 
 public class Fraction {
+	private int numerateur, denominateur;
 	
+	public Fraction(int num, int denom) {
+		numerateur = num;
+		denominateur = denom;
+	}
 	
-	
+	public String toString() {
+		return "la fraction de "+numerateur +"/"+denominateur +" est egale "+ numerateur/denominateur;
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Fraction f1 = new Fraction(20,2);
 		
+		System.out.println(f1);
 
 	}
 
